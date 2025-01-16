f7655d4a48b9906281024a424a82ef21d7881e82
AdminGeHfer <admin@gehfer.com.br>
2025-01-16 09:59:42 -0300

    Reapply "Promote main branch to production"
    This reverts commit a242925eca7ccc9abe157227a43e3726c1f3bf96.

--- src/components/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import * as React from "react";
-
-createRoot(document.getElementById("root")!).render(<App />);
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 import * as React from "react";
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-const container = document.getElementById('root');
-
-if (container) {
-  console.log('Initializing application...');
-  createRoot(container).render(<App />);
-} else {
-  console.error('Root element not found!');
-}
+createRoot(document.getElementById("root")!).render(<App />);
