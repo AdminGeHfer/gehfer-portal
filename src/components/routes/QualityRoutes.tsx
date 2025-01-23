f3e98133d25677bf90dffbb5792310597b8f9a1e
AdminGeHfer <admin@gehfer.com.br>
2025-01-23 10:59:17 -0300

    remove commented routes

--- src/components/routes/QualityRoutes.tsx
+++ src/components/routes/QualityRoutes.tsx
@@ -1,18 +1,11 @@
 import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-// const RNCList = lazy(() => import("@/pages/quality/RNCList"));
-// const RNCDetail = lazy(() => import("@/pages/quality/RNCDetail"));
-// const Dashboard = lazy(() => import("@/pages/quality/Dashboard"));
 const WorkflowEditor = lazy(() => import("@/pages/quality/workflow/WorkflowEditor"));
 
 const QualityRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Navigate to="/quality/dashboard" replace />} /> */}
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-      {/* <Route path="/rnc" element={<RNCList />} /> */}
-      {/* <Route path="/rnc/:id" element={<RNCDetail />} /> */}
       <Route path="/workflow" element={<WorkflowEditor />} />
     </Routes>
   );
