de9a49c37f2f1a3de96583836ce3a63b86b8c407
AdminGeHfer <admin@gehfer.com.br>
2025-01-27 16:50:48 -0300

    update contato layout

--- src/components/rnc/tabs/details/RelationalInfoTab.tsx
+++ src/components/rnc/tabs/details/RelationalInfoTab.tsx
@@ -143,7 +143,7 @@ export const RelationalInfoTab = React.forwardRef<RelationalInfoTabRef, Relation
         <div>
           <h3 className="text-lg font-semibold mb-4">Contato</h3>
           <Form {...form}>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <FormField
                 control={form.control}
                 name="name"
