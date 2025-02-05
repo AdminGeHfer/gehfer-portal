7f273e1eedfbd1b9838ce93e8cad480c123a27d4
AdminGeHfer <admin@gehfer.com.br>
2025-02-05 08:55:52 -0300

    add lovable preview

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "94ed982c-90e4-489c-8b62-1feafa1eadbb.lovableproject.com"
+    ]
   },
   plugins: [
     react(),
