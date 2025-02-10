0f0076f79c514c733cc963516f40139b38b86d1f
AdminGeHfer <admin@gehfer.com.br>
2025-02-10 13:41:43 -0300

    add green background on success messages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,10 @@ export default {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
